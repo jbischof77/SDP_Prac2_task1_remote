0885eb2ca7cb5d8643577f1d97343f637e79b993
Jonah <jonahbischof1@gmail.com>
2021-05-18 13:49:57 +0200

    Tests added to UnitTest1.cs

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -14,5 +14,33 @@ namespace Oware.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void ResetHouseRestoresHouseToTheSameState()
+        {
+            //ARRANGE
+            House h = new House(0, 0);
+            for (int i = 0; i < 16; i++)
+            {
+                h.AddSeedInPot(new Seed());
+            }
+            //ACT
+            h.ResetHouse();
+            //ASSERT
+            Assert.AreEqual(4, h.GetCount());
+        }
+
+        [Test]
+        public void AddSeedToScoreCallsAddSeed()
+        {
+            //ARRANGE
+            Mock<ScoreHouse> sch = new Mock<ScoreHouse>();
+            Player player = new Player("Jonah");
+            //ACT
+            sch.Setup(x => x.AddSeed(new Seed()));
+            player.AddSeedToScoreHouse(new Seed());
+            //ASSERT
+            sch.VerifyAll();
+        }
     }
 }
